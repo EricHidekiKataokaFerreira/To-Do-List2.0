85ecd207bdde9d8751fbe94d8ddccdf1be940d43
João Carlos <joaocarlosjoe74@gmail.com>
2022-08-22 17:03:06 -0300

    code

--- todolist/index.js
+++ todolist/index.js
@@ -1,5 +1,15 @@
-var texto = document.querySelector('#input-text');
-var containerItem = document.querySelector('todo__todo-item');
-function addItem() {
-    containerItem.appendChild(texto);
+var containerItem = document.querySelector('todo__todo-container-item');
+var banco = [];
+var getBanco = function () { var _a; return (_a = JSON.parse(localStorage.getItem('todo-list'))) !== null && _a !== void 0 ? _a : []; };
+var setBanco = function (banco) { return localStorage.setItem('todo-list', JSON.stringify(banco)); };
+function criarItem() {
+}
+function AddItem() {
+    var texto = document.querySelector('#input-text').value;
+    var item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = "<input type=\"checkbox\"> <div\">".concat(texto, "</div> <input type=\"button\" value=\"excluir\" onClick=\"").concat(RemoveItem(item), "\">");
+    document.getElementById('todo-list').appendChild(item);
+}
+function RemoveItem(indice) {
 }
--- todolist/index.ts
+++ todolist/index.ts
@@ -1,6 +1,29 @@
-let texto = document.querySelector('#input-text')
-let containerItem = document.querySelector('todo__todo-item')
+let containerItem = document.querySelector('todo__todo-container-item')
 
-function addItem(){
-  containerItem.appendChild(texto)
+let banco = []
+
+const getBanco = () => JSON.parse(localStorage.getItem('todo-list')) ?? []
+
+const setBanco = (banco) => localStorage.setItem('todo-list', JSON.stringify(banco))
+
+function criarItem() {
+
+}
+
+
+
+function AddItem(){
+  var texto =  (<HTMLSelectElement>document.querySelector('#input-text')).value
+  
+  const item = document.createElement('label')
+  item.classList.add('todo__item')
+  
+  item.innerHTML = `<input type="checkbox"> <div">${texto}</div> <input type="button" value="excluir" onClick="${RemoveItem(item)}">`
+
+  document.getElementById('todo-list').appendChild(item)
+  
+}
+
+function RemoveItem(indice) {
+
 }
