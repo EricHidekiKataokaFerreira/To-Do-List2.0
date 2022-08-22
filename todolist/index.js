a331b31a702cf1908b0425bf1fb87871a4677f9d
Eric Hideki <kataoka.eric@gmail.com>
2022-08-22 17:59:52 -0300

    tentei terminar mas n deu

--- todolist/index.js
+++ todolist/index.js
@@ -2,14 +2,33 @@ var containerItem = document.querySelector('todo__todo-container-item');
 var banco = [];
 var getBanco = function () { var _a; return (_a = JSON.parse(localStorage.getItem('todo-list'))) !== null && _a !== void 0 ? _a : []; };
 var setBanco = function (banco) { return localStorage.setItem('todo-list', JSON.stringify(banco)); };
-function criarItem() {
-}
-function AddItem() {
+function AddItem(tarefa, status, indice) {
+    var todoList = document.getElementById('todoList');
     var texto = document.querySelector('#input-text').value;
     var item = document.createElement('label');
     item.classList.add('todo__item');
-    item.innerHTML = "<input type=\"checkbox\"> <div\">".concat(texto, "</div> <input type=\"button\" value=\"excluir\" onClick=\"").concat(RemoveItem(item), "\">");
+    item.innerHTML = "<input type=\"checkbox\"> <div\">".concat(texto, "</div> <input id='remove-item' type=\"button\" value=\"excluir\">");
     document.getElementById('todo-list').appendChild(item);
+    banco.push({ 'tarefa': texto, 'status': '' });
+    setBanco(banco);
+    refreshScreen(todoList);
+    console.log(banco);
 }
-function RemoveItem(indice) {
-}
+var RemoveItem = function (evento) {
+    var elemento = evento.target;
+    var indice = elemento.dataset.indice;
+    var todoList = document.getElementById('todoList');
+    var banco = getBanco();
+    console.log('banco', banco);
+    banco.splice(indice, 1);
+    setBanco(banco);
+    refreshScreen(todoList);
+};
+var refreshScreen = function (todoList) {
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+    var banco = getBanco();
+    banco.forEach(function (item, indice) { return AddItem(item.tarefa, item.status, indice); });
+};
+document.getElementById('remove-item').addEventListener('keypress', RemoveItem);
--- todolist/index.ts
+++ todolist/index.ts
@@ -6,24 +6,40 @@ const getBanco = () => JSON.parse(localStorage.getItem('todo-list')) ?? []
 
 const setBanco = (banco) => localStorage.setItem('todo-list', JSON.stringify(banco))
 
-function criarItem() {
+function AddItem(tarefa: string, status: string, indice: string){
+  const todoList = document.getElementById('todoList')
 
-}
-
-
-
-function AddItem(){
   var texto =  (<HTMLSelectElement>document.querySelector('#input-text')).value
   
   const item = document.createElement('label')
   item.classList.add('todo__item')
   
-  item.innerHTML = `<input type="checkbox"> <div">${texto}</div> <input type="button" value="excluir" onClick="${RemoveItem(item)}">`
+  item.innerHTML = `<input type="checkbox"> <div">${texto}</div> <input id='remove-item' type="button" value="excluir">`
 
   document.getElementById('todo-list').appendChild(item)
   
+  banco.push ({'tarefa': texto, 'status': ''})
+  setBanco(banco)
+  refreshScreen(todoList)
+  console.log(banco)
 }
 
-function RemoveItem(indice) {
+const RemoveItem = (evento) => {
+  const elemento = evento.target
+  const indice = elemento.dataset.indice
+  const todoList = document.getElementById('todoList')
+  const banco = getBanco()
 
+  console.log('banco', banco)
+  banco.splice (indice, 1)
+  setBanco(banco)
+  refreshScreen(todoList)
 }
+
+const refreshScreen = (todoList) => {
+  while (todoList.firstChild) { todoList.removeChild(todoList.lastChild) }
+  const banco = getBanco()
+  banco.forEach ( (item, indice) => AddItem (item.tarefa, item.status, indice))
+}
+
+document.getElementById('remove-item').addEventListener('keypress', RemoveItem)
